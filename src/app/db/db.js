e584a044beeb2ce6981d2cfca058f2ddbfb2de6d
mouseinyl <rmaaron11@gmail.com>
2020-09-25 01:49:56 -0400

    create database fixed error invalid space

--- src/app/db/db.js
+++
@@ -1,8 +0,0 @@
-export const data = [
-    { "nombre": "alan", "apellido": "tomate", "telefonos": [{ "telefono": '80966733514' }, { "telefono": '80966733514' }] },
-    { "nombre": "marcos", "apellido": "aguacate", "telefonos": [{ "telefono": '8099996666' }, { "telefono": '8099996666' }, { "telefono": '8099996666' }, { "telefono": '8099996666' }] },
-    { "nombre": "pedro", "apellido": "queso", "telefonos": [{ "telefono": '8099996666' }, { "telefono": '8099996666' }, { "telefono": '8099996666' }, { "telefono": '8099996666' }] },
-    { "nombre": "luis", "apellido": "perez", "telefonos": [{ "telefono": '8099996666' }, { "telefono": '8099996666' }, { "telefono": '8099996666' }, { "telefono": '8099996666' }] },
-    { "nombre": "manuel", "apellido": "roman", "telefonos": [{ "telefono": '8099996666' }, { "telefono": '8099996666' }, { "telefono": '8099996666' }, { "telefono": '8099996666' }] },
-    { "nombre": "alan", "apellido": "hidalgo", "telefonos": [] }
-]
---
+++ src/app/db/db.ts
@@ -0,0 +1,8 @@
+export  const data = [
+    { "nombre": "alan", "apellido": "tomate", "telefono": [{ "tel": '80966733514' }, { "tel": '80966733514' }] },
+    { "nombre": "marcos", "apellido": "aguacate", "telefono": [{ "tel": '8099996666' }, { "tel": '8099996666' }, { "tel": '8099996666' }, { "tel": '8099996666' }] },
+    { "nombre": "pedro", "apellido": "queso", "telefono": [{ "tel": '8099996666' }, { "tel": '8099996666' }, { "tel": '8099996666' }, { "tel": '8099996666' }] },
+    { "nombre": "luis", "apellido": "perez", "telefono": [{ "tel": '8099996666' }, { "tel": '8099996666' }, { "tel": '8099996666' }, { "tel": '8099996666' }] },
+    { "nombre": "manuel", "apellido": "roman", "telefono": [{ "tel": '8099996666' }, { "tel": '8099996666' }, { "tel": '8099996666' }, { "tel": '8099996666' }] },
+    { "nombre": "alan", "apellido": "hidalgo", "telefono": [] }
+]
