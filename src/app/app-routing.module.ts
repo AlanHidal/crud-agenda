6062cde9eb8dc3c8f4e99e0c18de4d25fd3faf39
mouseinyl <rmaaron11@gmail.com>
2020-09-25 00:28:57 -0400

    add routes in router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
+import { TelefonoComponent } from './componetes/telefono/telefono.component';
+
+import { HomeComponent } from './componetes/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'home', component:HomeComponent},
+{ path: 'telefonos', component:TelefonoComponent},
+{ path: '', pathMatch: 'full', redirectTo: 'home' },
+{ path: '**', pathMatch: 'full', redirectTo: 'home' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
