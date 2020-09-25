d80288a7fa1047d11574557982824cebff2415e2
mouseinyl <rmaaron11@gmail.com>
2020-09-25 13:11:42 -0400

    add methos {del} on page home add estrutura html

--- src/app/componetes/home/home.component.ts
+++ src/app/componetes/home/home.component.ts
@@ -1,15 +1,21 @@
+import { contacto } from './../../model/contacto';
+import { ApiService } from './../../servicio/api.service';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class HomeComponent  {
+  Listcontact:contacto []= [];
+  constructor(private api:ApiService) {
+    this.Listcontact = this.api.GetListContact();
+   }
+   del(id){
+     this.api.RemoveContact(id);
+     
+   }
 
 }
